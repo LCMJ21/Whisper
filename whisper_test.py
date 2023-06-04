ea088840f495b0320d8f9a51819bbc447f1e4447
LCMJ21 <jorge.lima.ne@gmail.com>
2023-06-04 21:18:11 +0100

    goals added

--- whisper_test.py
+++ whisper_test.py
@@ -1,5 +1,5 @@
 import whisper
 
 model = whisper.load_model("base")
-result = model.transcribe("audio.mp3", fp16=False)
-print(result["text"])
+result = model.transcribe("audio.mp3")
+print(result)
