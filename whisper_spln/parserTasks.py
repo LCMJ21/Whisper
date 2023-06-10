63e6aff21c9bd558c462209d72b5bd62889af507
“Pierini10” <pierini10@gmail.com>
2023-06-10 15:16:44 +0100

    Add initial print

--- whisper_spln/parserTasks.py
+++ whisper_spln/parserTasks.py
@@ -19,15 +19,31 @@ def runWhisper(input_file, dest_folder, inputLang, outputLang):
         send_request = pickle.dumps(dict)
         client_socket.sendall(send_request)
         response = client_socket.recv(1024)
-        print('Received:', response.decode())
+        print(response.decode())
         client_socket.close()
     except ConnectionRefusedError:
         script_dir = os.path.dirname(os.path.abspath(__file__))
         error_log_path = os.path.join(script_dir, 'conf/error.log')
         output_log_path = os.path.join(script_dir, 'conf/output.log')
+        config_file_path = os.path.join(script_dir, 'conf/exec_time')
+        
+        prediction = predictTime(config_file_path, input_file)
+        print(f'Your file will be ready in {round(prediction, 2)} seconds!')
         subprocess.Popen(
             ["python3", "whisper_spln/startThreads.py", " ".join(sys.argv[1:])], stdout=open(output_log_path, "w"), stderr=open(error_log_path, "w"))
 
+
+def predictTime(file_time, input_file):
+    with open(file_time, 'r') as file:
+        lines = [line.rstrip() for line in file]
+    
+    meanTime = 0
+    if len(lines) == 2:
+        meanTime = float(lines[0])
+    
+    size = os.stat(input_file).st_size
+    return size * meanTime
+        
 
 def getQueue():
     try:
